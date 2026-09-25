df8302d3c4bac744d78f686b3ced1981c8ba15e5
Deadrojer <139552280+Deadrojer@users.noreply.github.com>
2026-09-25 23:29:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -25,11 +25,11 @@ class Config:
     # ── Telegram ──────────────────────────────────────────────────────────────
     API_ID         = int(os.environ.get("API_ID", "20140875"))
     API_HASH       = os.environ.get("API_HASH", "a06fa97d5a853ec2da79015b11335a17")
-    BOT_TOKEN      = os.environ.get("BOT_TOKEN", "8665494682:AAHDXdA0EvGybrB5OPT2uImwO6I6_fp0giY")
+    BOT_TOKEN      = os.environ.get("BOT_TOKEN", "")
     BOT_MAX_SIZE = int(os.environ.get("BOT_MAX_SIZE", str(2000 * 1024 * 1024)))
     # Shared String Session — used for reading/writing Worker Control Group
     # protocol messages. NEVER used to download or upload files.
-    STRING_SESSION = os.environ.get("STRING_SESSION", "BQEzvOwAp8qnAYyXypZU0mR_HNFp0okIC1Wih7RxRVshNuwW3Qr0XD27YMNg1irrSKrwvS_2jQWK_XROaTNGVid7cUCN6IH3Rz1sqyTLI2y4rfPz32b5K3AaA_APHe0MKbxqq45ksogn8y5G-qCfq4QVuvZsJT2uHTu-OtAd0Q2caM8B3YQe0QLtysUy5mQFJ9E2kNOrhfF65Y2gRaxvooF8Mz-3EaRt7F47i1AzmsJA5fUPgLO3L9zvWnKHe5OPxNZH3v4KsMzQIibUrVwFep4qrUTPRzyr19gwTITUN1SR0j_E7m8kGQZaXVvKJYUXUQ5GY0pSr_hcq9iwMFfLhgjCHDkOhAAAAAF8DWVvAA")
+    STRING_SESSION = os.environ.get("STRING_SESSION", "")
 
     # ── Channels / Groups ─────────────────────────────────────────────────────
     WORKER_CONTROL_GROUP_ID  = int(os.environ.get("WORKER_CONTROL_GROUP_ID",  "-1004454437880"))
@@ -38,7 +38,7 @@ class Config:
     LOG_CHANNEL              = int(os.environ.get("LOG_CHANNEL", "-1004427602817"))
 
     # ── Database ──────────────────────────────────────────────────────────────
-    MONGO_URI = os.environ.get("MONGO_URI", "mongodb://devheroku69_db_user:q7Prl5Mczqx1RHnV@ac-qc1reu1-shard-00-00.em2s7v5.mongodb.net:27017,ac-qc1reu1-shard-00-01.em2s7v5.mongodb.net:27017,ac-qc1reu1-shard-00-02.em2s7v5.mongodb.net:27017/?ssl=true&replicaSet=atlas-5roaph-shard-0&authSource=admin&appName=Devheroku69")
+    MONGO_URI = os.environ.get("MONGO_URI", "")
     DB_NAME   = os.environ.get("DB_NAME", "DistributedRenameBot")
 
     # ── Owners / Admins ───────────────────────────────────────────────────────
